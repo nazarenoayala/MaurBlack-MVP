140033229de555d204dd32c4cb4e9ce89b5f5265
nazarenoayala <nazaayala1@gmail.com>
2026-03-09 21:41:52 +0100

    fix bookingModal

--- client/src/components/FlashBookingModal/FlashBookingModal.jsx
+++ client/src/components/FlashBookingModal/FlashBookingModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import api from '../../../services/api';
+import api from '../../services/api';
 import './flashBookingModal.css';
 
 const FlashBookingModal = ({flash, onClose}) => {
