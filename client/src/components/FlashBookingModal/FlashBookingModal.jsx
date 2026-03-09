6ef5ffa8d1db9ecae96fa20947857aa688c966e7
nazarenoayala <nazaayala1@gmail.com>
2026-03-09 20:55:03 +0100

    fix css case sensitivity

--- client/src/components/FlashBookingModal/FlashBookingModal.jsx
+++ client/src/components/FlashBookingModal/FlashBookingModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios';
-import './FlashBookingModal.css';
+import './flashBookingModal.css';
 
 const FlashBookingModal = ({flash, onClose}) => {
     const [formFlash, setFormFlash] = useState({
